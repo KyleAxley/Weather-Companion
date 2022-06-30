86c8fb14174b1a687e77422b65682750d84a524c
Your Name <you@example.com>
2022-06-30 16:52:24 -0400

    comments made/testing run

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,45 +12,36 @@ $(document).ready(function () {
     var windEl = $('span#wind');
     var uvIndexEl = $('span#uv-index');
     var cityListEl = $('div.cityList');
+    var cityInput = $('#city-input');
 
-   // Selectors for form elements
-   var cityInput = $('#city-input');
-
-   // Store past searched cities
-   let pastCities = [];
-
-
-    // Load events from local storage
+    // local storage funtions
     function loadCities() {
         var storedCities = JSON.parse(localStorage.getItem('pastCities'));
         if (storedCities) {
             pastCities = storedCities;
         }
     }
-
-    // Store searched cities in local storage
     function storeCities() {
         localStorage.setItem('pastCities', JSON.stringify(pastCities));
     }
+    let pastCities = [];
 
    // Functions to build the URL for the OpenWeather API call
- 
-    function buildURLFromInputs(city) {
+    function openWeatherInput(city) {
         if (city) {
             return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         }
     }
 
-    function buildURLFromId(id) {
+    function openWeatherId(id) {
         return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apiKey}`;
     }
 
-     // Function to display the last 5 searched cities
+     //display recently searched cities(5)
      function displayCities(pastCities) {
         cityListEl.empty();
         pastCities.splice(5);
         let sortedCities = [...pastCities];
-        //sortedCities.sort(compare);
         sortedCities.forEach(function (location) {
             let cityDiv = $('<div>').addClass('col-12 city');
             let cityBtn = $('<button>').addClass('btn btn-light city-btn').text(location.city);
@@ -59,26 +50,13 @@ $(document).ready(function () {
         });
     }
     
-    function setUVIndexColor(uvi) {
-        if (uvi < 3) {
-            return 'green';
-        } else if (uvi >= 3 && uvi < 6) {
-            return 'yellow';
-        } else if (uvi >= 6 && uvi < 8) {
-            return 'orange';
-        } else if (uvi >= 8 && uvi < 11) {
-            return 'red';
-        } else return 'purple';
-    }
-
-
+    
     function searchWeather(queryURL) {
-
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function (response) {
-
+            
             // Store current city in past cities
             let city = response.name;
             let id = response.id;
@@ -101,8 +79,8 @@ $(document).ready(function () {
             temperatureEl.html(((response.main.temp - 273.15) * 1.8 + 32).toFixed(1));
             humidityEl.text(response.main.humidity);
             windEl.text((response.wind.speed * 2.237).toFixed(1));
-
-            // Call OpenWeather API OneCall with lat and lon to get the UV index and 5 day forecast
+            
+            // Call OpenWeather API with lat and lon to get the UV index and 5 day forecast
             let lat = response.coord.lat;
             let lon = response.coord.lon;
             let queryURLAll = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
@@ -115,7 +93,7 @@ $(document).ready(function () {
                 uvIndexEl.text(response.current.uvi);
                 uvIndexEl.attr('style', `background-color: ${uvColor}; color: ${uvColor === "yellow" ? "black" : "white"}`);
                 let fiveDay = response.daily;
-
+                
                 // Display 5 day forecast in DOM elements
                 for (let i = 0; i <= 5; i++) {
                     let currDay = fiveDay[i];
@@ -123,45 +101,56 @@ $(document).ready(function () {
                     $(`div.day-${i} .fiveDay-img`).attr(
                         'src',
                         `http://openweathermap.org/img/wn/${currDay.weather[0].icon}.png`
-                    ).attr('alt', currDay.weather[0].description);
-                    $(`div.day-${i} .fiveDay-temp`).text(((currDay.temp.day - 273.15) * 1.8 + 32).toFixed(1));
-                    $(`div.day-${i} .fiveDay-humid`).text(currDay.humidity);
-                }
+                        ).attr('alt', currDay.weather[0].description);
+                        $(`div.day-${i} .fiveDay-temp`).text(((currDay.temp.day - 273.15) * 1.8 + 32).toFixed(1));
+                        $(`div.day-${i} .fiveDay-humid`).text(currDay.humidity);
+                    }
+                });
             });
-        });
-    }
-
-    // Click handler for search button
-    $('#search-btn').on('click', function (event) {
-        // Preventing the button from trying to submit the form
-        event.preventDefault();
-        let city = cityInput.val().trim();
-        city = city.replace(' ', '%20');
-        cityInput.val('');
-
-        // Build the query url with the city and searchWeather
-        if (city) {
-            let queryURL = buildURLFromInputs(city);
-            searchWeather(queryURL);
-        } else {
-            let queryURL = buildURLFromInputs("cleveland");
-            searchWeather(queryURL);
         }
-    }); 
-    
-    // Click handler for city buttons to load that city's weather
-    $(document).on("click", "button.city-btn", function (event) {
-        let clickedCity = $(this).text();
-        let foundCity = $.grep(pastCities, function (storedCity) {
-            return clickedCity === storedCity.city;
-        })
-        let queryURL = buildURLFromId(foundCity[0].id)
-        searchWeather(queryURL);
-    });
-
-    loadCities();
-    displayCities(pastCities);
-
+        //function to set color for the generated UV index
+        function setUVIndexColor(uvi) {
+            if (uvi < 3) {
+                return 'green';
+            } else if (uvi >= 3 && uvi < 6) {
+                return 'yellow';
+            } else if (uvi >= 6 && uvi < 8) {
+                return 'orange';
+            } else if (uvi >= 8 && uvi < 11) {
+                return 'red';
+            } else return 'purple';
+        }
+        // Click handler for search button
+        $('#search-btn').on('click', function (event) {
+            // Preventing the button from trying to submit the form
+            event.preventDefault();
+            let city = cityInput.val().trim();
+            city = city.replace(' ', '%20');
+            cityInput.val('');
+            
+            //generate the city and weather query with default set to my gome city "cleveland"
+            if (city) {
+                let queryURL = openWeatherInput(city);
+                searchWeather(queryURL);
+            } else {
+                let queryURL = openWeatherInput("cleveland");
+                searchWeather(queryURL);
+            }
+        }); 
+        
+        // Click handler for city buttons to load that city's weather
+        $(document).on("click", "button.city-btn", function (event) {
+            let clickedCity = $(this).text();
+            let foundCity = $.grep(pastCities, function (storedCity) {
+                return clickedCity === storedCity.city;
+            })
+            let queryURL = openWeatherId(foundCity[0].id)
+            searchWeather(queryURL);
+        });
+        
+        loadCities();
+        displayCities(pastCities);
+        
     displayLastSearchedCity();
 
 });
