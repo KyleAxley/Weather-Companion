ca35b6428cd65f0f04a3a6d3a2b2c340f176f83e
Your Name <you@example.com>
2022-06-30 00:44:37 -0400

    added local storage/saved city function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,22 @@ $(document).ready(function () {
     // My OpenWeather generated API key
     const apiKey = 'd96eaa93b402b86cc39119d34076f8b0';
 
-    
+
+    //local storage for saved city searches. 
+    function loadCities() {
+        var storedCity = JSON.parse(localStorage.getItem ("savedCities"));
+        if (storedCity) {
+            savedCities = storedCity;
+        }
+    }
+
+    let savedCities = [];
+
+    function storeCities() {
+        localStorage.setItem('savedCities', JSON.stringify(pastCities));
+    }
+
+    //Open Weather API functions. 
     function openWeatherInputs(city) {
         if(city) {
             return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
@@ -15,6 +30,16 @@ $(document).ready(function () {
     }
 
 
+    function displayCities(savedCities) {
+        cityListEl.empty();
+        savedCities.splice(5);
+        var sortedCities = [...savedCities];
+        sortedCities.forEach(function (location) {
+            var cityDiv = $('<div>').addClass('col-12 city');
+            var cityBtn = $('<button>').addClass('btn btn-light city-btn').text(location.city);
+            cityListEl.append(cityDiv);
+        });
+    }
 
     function searchWeather(queryURL) {
         $ajax({
@@ -25,7 +50,7 @@ $(document).ready(function () {
             let id = response.id;
             
             if (pastCities[0]) {
-                pastCities = $.grep(pastCitites, function (storedCity) {
+                pastCities = $.grep(savedCities, function (storedCity) {
                     return id !== storedCity.id;
                 })
             }
